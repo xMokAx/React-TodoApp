975829c3ea45e9b160673c15405a932d5296fc91
xMokAx <ahmedmokhtar11j@gmail.com>
2017-10-27 02:36:20 +0200

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './css/reset.min.css'
-import './css/style.css'
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
